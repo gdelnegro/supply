041a42adbacc679477ca30123a7243639225a599
gdelnegro <gdelnegro@fedora-gustavo.home>
2014-03-19 16:37:59 -0300

    Adicionando ACL

--- application/modules/admin/Bootstrap.php
+++ application/modules/admin/Bootstrap.php
@@ -2,5 +2,9 @@
 
 class Admin_Bootstrap extends Zend_Application_Module_Bootstrap
 {
-    
+    public function _initAutoload(){
+        $autoloader = Zend_Loader_Autoloader::getInstance();
+        $autoloader->registerNamespace('Opi_');
+        return $autoloader;
+    }
 }
