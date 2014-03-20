32bbfb15a0529170017c3f8af0b7516b5f70e01d
gdelnegro <gdelnegro@fedora-gustavo.home>
2014-03-20 13:37:59 -0300

    Alterado layout informações do usuário

--- application/modules/admin/controllers/CadastroController.php
+++ application/modules/admin/controllers/CadastroController.php
@@ -13,10 +13,14 @@ class Admin_CadastroController extends Zend_Controller_Action
         /*
          * Edita as informações do usuário logado
          */
+        $usuario = new Admin_Model_Usuario();    
+        $dados = $usuario->pesquisaUsuario(1,true);
+        $this->view->dados = $dados;
+    }
+    
+    public function editAction(){
         $usuario = new Admin_Model_Usuario();
-        
-        $dados = $usuario->pesquisaUsuario(1);
-        
+        $dados = $usuario->pesquisaUsuario($this->_getParam('id'));
         $this->view->dados = $dados;
     }
 
--- application/modules/admin/models/Usuario.php
+++ application/modules/admin/models/Usuario.php
@@ -6,19 +6,27 @@ class Admin_Model_Usuario
     protected $id;
     public $email;
     
-    public function pesquisaUsuario($id){
-            $dbPessoaEndereco = new Admin_Model_DbTable_PessoaEndereco();
-            $dbEndereco = new Admin_Model_DbTable_Endereco();
-            $dbPessoa = new Admin_Model_DbTable_Pessoa();
-            $dbTipoEndereco = new Admin_Model_DbTable_TipoEndereco();
+    public function pesquisaUsuario($id,$end = false){
+            $dbPessoa = new Admin_Model_DbTable_Pessoa();            
             
             $selectPessoa = $dbPessoa->select()
                     ->from('pessoa')
                     ->where('id = ?', $id);
             $stmtPessoa = $selectPessoa->query();
             $dadosPessoa = $stmtPessoa->fetchAll();
-            
-            $selectPessoaEndereco = $dbPessoaEndereco->select()
+
+            if($end==true){
+                $dadosPessoa[0]['enderecos']=$this->pesquisaEndereco($id);
+            }
+            return $dadosPessoa[0];
+    }
+    
+    public function pesquisaEndereco($id){
+        $dbPessoaEndereco = new Admin_Model_DbTable_PessoaEndereco();
+        $dbEndereco = new Admin_Model_DbTable_Endereco();
+        $dbTipoEndereco = new Admin_Model_DbTable_TipoEndereco();
+        
+        $selectPessoaEndereco = $dbPessoaEndereco->select()
                     ->from('pessoaEndereco')
                     ->where('pessoa = ?', $id);
             $stmtPessoaEndereco = $selectPessoaEndereco->query();
@@ -41,11 +49,10 @@ class Admin_Model_Usuario
                 $tipoEndereco = $stmtTipoEndereco->fetchAll();
                 $enderecos[$i]=$endereco[0];
                 $enderecos[$i]['tipoEndereco'] = $tipoEndereco[0]['descricao'];
-            }            
-
-            $dadosPessoa[0]['enderecos']=$enderecos;
-            return $dadosPessoa[0];
-    }    
+            }
+            return $enderecos;
+    }
+    
 
 }
 
