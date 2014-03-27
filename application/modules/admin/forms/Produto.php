04c96d4da94c2e0b4abe1ea558527c872fa98c62
gdelnegro <gdelnegro@fedora-gustavo>
2014-03-27 11:42:02 -0300

    Editado menu

---
+++ application/modules/admin/forms/Produto.php
@@ -0,0 +1,55 @@
+<?php
+
+class Admin_Form_Produto extends Twitter_Form
+{
+   
+    private $_tipo;
+    protected $_editavel;
+    protected $_usr;
+    
+    public function __construct($tipo, $usr,$options = null) {
+        if(strtoupper($tipo) == 'EDIT' OR strtoupper($tipo) == 'NEW'){
+            $this->_editavel = true;
+        }else{
+            $this->_editavel = false;
+        }
+        if($usr == 1){
+            $this->_usr = 'admin';
+        }
+        $this->_tipo = $tipo;
+        parent::__construct($options);
+    }
+    
+    public function init()
+    {
+        $this->setMethod('post');
+        $this->setAttrib('class','form-sigin');
+        $this->setAttrib('role','form');
+        
+        /* Form Elements & Other Definitions Here ... */
+        $id = new Zend_Form_Element_Hidden('id');
+        
+        $descricao = new Zend_Form_Element_Text('descricao');
+        $descricao->setLabel('Descricao')                
+                ->setAttrib('placeholder','Descricao')
+                ->removeDecorator('HtmlTag');
+        
+        
+        $this->addElements(array(
+            $id,
+            $descricao,
+        ));
+        
+        if($this->_usr == 'admin'){
+        }
+        
+        foreach($this->getElements() as $element ){
+            $element->setAttrib('class', 'form-control');
+        }
+        
+        if($this->_tipo == 'edit' OR $this->_tipo == 'new'){
+            $this->addElement('submit',   'Enviar',   array('ignore'    =>  true,));
+        }
+        
+    }
+}
